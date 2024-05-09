3006f55a3170fb278909eb1cbcf4a6e6b5a4e248
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-05-09 17:28:44 -0500

    Añadiendo pagina de loggin form junto con la logica inicial

---
+++ loginform.js
@@ -0,0 +1,8 @@
+
+const mostrarData = () =>{
+    const email = document.getElementById('email').value;
+    const contrasenia = document.getElementById('password').value;
+    return[email, contrasenia];
+}
+
+
