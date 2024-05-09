68cadd0a9f85deba5f22d14cc2500342a85821fe
DanielaMendozaA <danni.ddma.96@gmail.com>
2024-05-09 16:00:14 -0500

    add changes

--- app.js
+++ app.js
@@ -8,22 +8,46 @@ const options = {
   };
   
   
-  async function cargarDatos() {
-    try {
-      const response = await fetch(options.url, {
-        headers: {
-          'X-RapidAPI-Key': '7fe86bd6f3mshb7f8320f0f43933p18904djsnc940b4e03823',
-          'X-RapidAPI-Host': 'beers-list.p.rapidapi.com'
-        }
-      })
-      const data = await response.json();
-      console.table(data);
-    } catch (error) {
-      console.error(error);
-    }
-  
+async function cargarDatos() {
+  try {
+    const response = await fetch(options.url, {
+      headers: {
+        'X-RapidAPI-Key': '7fe86bd6f3mshb7f8320f0f43933p18904djsnc940b4e03823',
+        'X-RapidAPI-Host': 'beers-list.p.rapidapi.com'
+      }
+    })
+    const data = await response.json();
+    return data
+  } catch (error) {
+    console.error(error);
   }
-  
-  cargarDatos()
 
-  alert("hola")
+}
+
+let arrayDatos = []
+
+const main = (data) => {
+  arrayDatos = data
+  console.log(arrayDatos)
+
+
+
+
+}
+
+
+
+
+async function modificarDatos(){
+  try{
+    const respuesta = await cargarDatos()
+    main(respuesta)
+
+  }catch(error){
+    console.log("Error al recoger los datos");
+  }
+}
+
+modificarDatos()
+
+
